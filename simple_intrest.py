8702ee543fef0921101db52bd051c00ae611d8e9
hanifff <skhanif2302@gmail.com>
2026-09-04 14:27:33 +0530

    simple intrest

---
+++ simple_intrest.py
@@ -0,0 +1,5 @@
+principle=float(input("Enter the principle amount: "))
+rate=float(input("Enter the rate of interest:% "))
+time=float(input("Enter the time period: "))
+simple_intrest=(principle*rate*time)/100
+print("The simple interest is:",simple_intrest)

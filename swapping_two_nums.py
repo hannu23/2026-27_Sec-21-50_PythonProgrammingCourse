51aa2ec358b140bc435519d6907f3d3b09cf7af7
hanifff <skhanif2302@gmail.com>
2026-09-04 14:26:13 +0530

    swapping

---
+++ swapping_two_nums.py
@@ -0,0 +1,9 @@
+
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+a=a+b
+b=a-b
+a=a-b
+print("After swapping:")
+print("First number (a):", a)
+print("Second number (b):", b)

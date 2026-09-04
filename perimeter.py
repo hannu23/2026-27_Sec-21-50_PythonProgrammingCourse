c84a8ab6485791c220a768b6b2b91a87c1314c8f
hannu23 <skhanif2302@gmail.com>
2026-09-04 14:22:54 +0530

    perimeter.py

---
+++ perimeter.py
@@ -0,0 +1,7 @@
+
+num1=int(input("enter the length of the rectangle: "))
+num2=int(input("enter the breadth of the rectangle: "))
+area=num1*num2
+perimeter=2*(num1+num2)
+print(area)
+print(perimeter)

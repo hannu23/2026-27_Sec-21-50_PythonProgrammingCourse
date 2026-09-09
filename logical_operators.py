5c21dd00abdfe957e8910e721d58d83a95a7af1a
hanifff <skhanif2302@gmail.com>
2026-09-09 11:40:20 +0530

    logical operator

---
+++ logical_operators.py
@@ -0,0 +1,14 @@
+# logical_operators.py for (and)
+a = 5
+result= a > 2 and a < 10
+print("result of ",a, ">", 2,"and", a, "<", 10,"is: ", result)
+
+# logical_operators.py for (or)
+b = 5
+result2 = b < 3 or b > 10
+print("result of ",b, "<", 3,"or", b, ">", 10,"is: ", result2)
+
+# logical_operators.py for (not)
+c = 5
+result3 = not(c > 3)
+print("result of not(",c, ">", 3,") is: ", result3)
